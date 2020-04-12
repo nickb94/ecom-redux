7d1cfb779de4e043e74876c2b34f6f9bd036353a
Lahchhsin <nishchhalbhardwaj94@gmail.com>
2020-04-13 02:09:23 +0530

    wrench

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,16 +1,20 @@
 import React from "react";
-import "./Registration.css";
 import SignIn from "../../components/SignIn/SignIn";
 import SignUp from "../../components/SignUp/SignUp";
+import {Footer} from "../../components/Footer/Footer"
+
+import "./Registration.css";
 
 const Registration = () => {
 
     return (
-
+        <>
         <div className="registration">
             <SignIn />
             <SignUp />
         </div>
+        <Footer />
+        </>
     );
 
 }
