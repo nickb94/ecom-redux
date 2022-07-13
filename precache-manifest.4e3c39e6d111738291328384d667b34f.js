d56d47ad3289e38a233405ee5ffdbe89cede2c8e
Nischal Bhardwaj <nischal.bhardwaj@tadigital.com>
2022-07-14 01:02:53 +0530

    Updates

---
+++ precache-manifest.4e3c39e6d111738291328384d667b34f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df327fa90481c9813ccb48b331f21b77",
+    "url": "/ecom-redux/index.html"
+  },
+  {
+    "revision": "d46913f2456902e4af3d",
+    "url": "/ecom-redux/static/css/main.b8864164.chunk.css"
+  },
+  {
+    "revision": "070218a71ac1ba3cd03c",
+    "url": "/ecom-redux/static/js/2.e5474836.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/ecom-redux/static/js/2.e5474836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46913f2456902e4af3d",
+    "url": "/ecom-redux/static/js/main.17991512.chunk.js"
+  },
+  {
+    "revision": "d7e2cb9af9639e3c3169",
+    "url": "/ecom-redux/static/js/runtime-main.10e9feed.js"
+  },
+  {
+    "revision": "94d3af0cf97f44d7a1f2028a96529cc2",
+    "url": "/ecom-redux/static/media/tag.94d3af0c.svg"
+  }
+]);
